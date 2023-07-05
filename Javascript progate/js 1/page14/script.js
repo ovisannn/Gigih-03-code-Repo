8c20204507d5bfa2bc1a947e0331321e4bfb058f
Ovi sanjaya <ovi.sanjaya11@gmail.com>
2023-07-06 06:17:58 +0700

    up

---
+++ Javascript progate/js 1/page14/script.js
@@ -0,0 +1,19 @@
+const password = "ninjaken";
+
+// Ketika nilai password adalah "ninjaken", cetak "Berhasil log in"
+if(password === "ninjaken"){
+  console.log("Berhasil log in");
+}
+else
+{
+  console.log("Password salah");
+}
+
+// Ketika nilai password bukan "ninjaken", cetak "Password salah"
+if(password !== "ninjaken"){
+  console.log("Password salah");
+}
+else
+{
+  console.log("Berhasil log in");
+}
