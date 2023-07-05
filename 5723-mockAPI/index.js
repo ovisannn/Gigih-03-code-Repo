2ba2d6af828853a380b1dcc25713af49dade345d
Ovi sanjaya <ovi.sanjaya11@gmail.com>
2023-07-06 05:24:25 +0700

    Update index.js

--- 5723-mockAPI/index.js
+++ 5723-mockAPI/index.js
@@ -8,9 +8,23 @@ const dummy =
   { title: "song title 3", artists: [{ name: "artist name 3"}], duration: 200 },
   { title: "song title 4", artists: [{ name: "artist name 4"}], duration: 200 }]
 
-app.get('/', function (req, res) {
-  res.send(dummy);
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// app.get('/', function (req, res) {
+
+// });
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
+
+let guest = Math.floor(Math.random()*5)
+let check = Math.floor(Math.random()*5)
+try{
+  if (guest == check){
+    // res.send(dummy)
+    console.log(dummy)
+  }else{
+    nonExistFunction()
+  }
+}catch(error){
+  console.error(error);
+}
